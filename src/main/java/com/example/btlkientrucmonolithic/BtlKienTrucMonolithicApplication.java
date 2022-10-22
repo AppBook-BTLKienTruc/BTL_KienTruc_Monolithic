88e0a32033645ecc354417a86295d0011dc9c329
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-22 10:51:46 +0700

    start BTL MyBook - JPA - Monolithic

---
+++ src/main/java/com/example/btlkientrucmonolithic/BtlKienTrucMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.example.btlkientrucmonolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlKienTrucMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlKienTrucMonolithicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/btlkientrucmonolithic/BtlKienTrucMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btlkientrucmonolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlKienTrucMonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
